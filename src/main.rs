e2e2b5c2a41a9e0e0f4abd3a5ea560ec8ed2ada3
Sabarish Sridhar <sabarish.sr@outlook.com>
2024-09-05 07:48:57 +0200

    Added TC

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 
 use reqwest;
 
-fn main() {
-     let resp = match reqwest::blocking::get("https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10") {
+fn fetchData(url: &str) {
+     let resp = match reqwest::blocking::get(url) {
          Ok(resp) => resp.text().unwrap(),
          Err(err) => panic!("Error: {}", err)
      };
-    println!("{}", resp)
+    println!("{}", resp);
 
+}
+
+fn main(){
+
+    fetchData("https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10");
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn test_validURL() {
+    //     assert!(fetchData("https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10"));
+    // }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_url() {
+        fetchData("SADASSSAD")
+    }
+
+
+    #[test]
+    fn test_valid_url() {
+        fetchData("https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10")
+    }
 }
