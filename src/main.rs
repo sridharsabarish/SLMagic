968a13eeeb8134929e9dc18e60bfa0716af739d7
Sabarish Sridhar <sabarish.sr@outlook.com>
2024-09-12 07:30:30 +0200

    Some brainstorming

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,14 @@ fn fetchData(url: &str) {
 }
 
 fn main(){
-
-    fetchData("https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10");
+    let url ="https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10";
+    fetchData(url);
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    // #[test]
-    // fn test_validURL() {
-    //     assert!(fetchData("https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10"));
-    // }
-
     #[test]
     #[should_panic]
     fn test_invalid_url() {
@@ -37,3 +31,17 @@ mod tests {
         fetchData("https://transport.integration.sl.se/v1/sites/5502/departures?forecast=10")
     }
 }
+
+
+/*
+
+Todo :
+
+Some Logic for finding the  route with least wait time .
+1) Use the API call to find the departure from Point A to Point B
+2) Get the wait times if any between the two departures
+3) Find the route with least travel Time and Departures.
+4) Do additional API calls from neighbouring stations if needed.
+5) Choose the one with least waiting time (Less than 10 minutes) and also least travel time.
+
+*/
